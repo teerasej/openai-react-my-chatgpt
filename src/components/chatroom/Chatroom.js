d90b08e772032727149320a8dfa282a5f1e746c5
teerasej <teerasej@gmail.com>
2023-02-27 17:24:49 +0700

    render bubbles from redux's state

--- src/components/chatroom/Chatroom.js
+++ src/components/chatroom/Chatroom.js
@@ -1,14 +1,21 @@
 
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import MessageBubble from './MessageBubble';
 function Chatroom() {
+
+  const publishPrompts = useSelector(state => state.publishPrompts);
+
+  const renderedPosts = publishPrompts.map(prompts => (
+    <MessageBubble sender={prompts.sender} message={prompts.message}/>
+  ))
+
   return (
     <Row>
         <Container>
           <div className="chatroom">
-            <MessageBubble sender={'user'} message={'How are you?'}/>
-            <MessageBubble sender={'GPT'} message={'Fine, thank you. How are you?'}/>
+            {renderedPosts}
           </div>
         </Container>
     </Row>
