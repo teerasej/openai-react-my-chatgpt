da7d044a0070b11a01ed9866823894f79d9d7c31
teerasej <teerasej@gmail.com>
2023-02-27 17:58:18 +0700

    get input from the form

--- src/components/promptinput/PromptInput.js
+++ src/components/promptinput/PromptInput.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Controller, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
 
 function PromptInput() {
+
+  const { control, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Row>
-        <Col>
-          <Form>
-            <Form.Group controlId="message">
-              <Form.Label>Message</Form.Label>
-              <Form.Control type="text" placeholder="Type your message here" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Send
-            </Button>
-          </Form>
-        </Col>
-      </Row>
+      <Col>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group controlId="message">
+            <Form.Label>Message</Form.Label>
+            <Controller
+              name="message"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field, fieldState: { error } }) => (
+                <Form.Control type="text" placeholder="Type your message here" isInvalid={!!error} {...field} />
+              )}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a message.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Send
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
