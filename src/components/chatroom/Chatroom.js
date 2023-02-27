c6e919849ec71a416aacab768aaddc5ada8df5a2
teerasej <teerasej@gmail.com>
2023-02-27 17:03:03 +0700

    provide mockup

--- src/components/chatroom/Chatroom.js
+++ src/components/chatroom/Chatroom.js
@@ -1,13 +1,17 @@
 
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import MessageBubble from './MessageBubble';
 function Chatroom() {
   return (
     <Row>
-        <Col>
-          <div className="chatroom"></div>
-        </Col>
-      </Row>
+        <Container>
+          <div className="chatroom">
+            <MessageBubble sender={'user'} message={'How are you?'}/>
+            <MessageBubble sender={'GPT'} message={'Fine, thank you. How are you?'}/>
+          </div>
+        </Container>
+    </Row>
   )
 }
 
