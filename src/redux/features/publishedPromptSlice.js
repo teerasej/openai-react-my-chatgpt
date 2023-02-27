b6fe08d22d08720a6761eb3676bbc17375395f47
teerasej <teerasej@gmail.com>
2023-02-27 18:37:30 +0700

    post prompt to openAI with new action

--- src/redux/features/publishedPromptSlice.js
+++ src/redux/features/publishedPromptSlice.js
@@ -1,10 +1,47 @@
 
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios';
+import key from './openAPI'
+
 
 const initialState = [
   { id: '1', sender: 'User', message: 'Hello!' },
   { id: '2', sender: 'GPT', message: 'How ya!' }
 ]
+
+export const fetchOpenAI = createAsyncThunk(
+  'user/fetchOpenAI',
+  async (prompt) => {
+
+    console.log('fetching openAI')
+
+    const jsonPrompt = JSON.stringify({
+      "model": "text-davinci-003",
+      "prompt": `${prompt}\n\n>`,
+      "temperature": 0.7,
+      "max_tokens": 256,
+      "top_p": 1,
+      "frequency_penalty": 0,
+      "presence_penalty": 0
+    });
+
+    console.log(jsonPrompt)
+
+    const response = await axios.post(
+      'https://api.openai.com/v1/completions',
+      jsonPrompt,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        }
+      });
+
+
+    return response.data.choices[0].text;
+  }
+);
+
 
 const publishedPromptSlice = createSlice({
   name: 'publishedPrompts',
@@ -13,7 +50,21 @@ const publishedPromptSlice = createSlice({
     promptAdded(state, action) {
       state.push(action.payload)
     },
-  }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchOpenAI.fulfilled, (state, action) => {
+        console.log('succeeded');
+        state.push({
+          sender: 'GPT',
+          message: action.payload
+        })
+      })
+      .addCase(fetchOpenAI.rejected, (state, action) => {
+        console.log('failed');
+        console.error(action.error.message);
+      });
+  },
 });
 
 export const { promptAdded } = publishedPromptSlice.actions
