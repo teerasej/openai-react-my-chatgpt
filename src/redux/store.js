201c76841dc03a241300b5dec1776ff510801d0f
teerasej <teerasej@gmail.com>
2023-02-27 17:21:14 +0700

    add to reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
+
 import { configureStore } from '@reduxjs/toolkit'
+import publishPromptReducer from './features/publishedPromptSlice'
 
 export default configureStore({
   reducer: {
+    publishPrompts: publishPromptReducer
   }
 })
